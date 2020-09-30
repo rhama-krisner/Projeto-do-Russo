5df4ce3f47e8c0e8387f1dc48e1c413933edb5f4
Rhama Krisner <rhama.krisner@hotmail.com>
2020-09-30 09:30:05 -0300

    Update

--- script/script.js
+++ script/script.js
@@ -1,21 +1,24 @@
-var contador = 0
-        var div = document.querySelector('.intelligentDiv')
-        
-        var id = null
+var contador = 1
+var div = document.querySelector('.intelligentDiv')
+var id = null
 
-        function play() {
-            if (id == null) {
-                id = setInterval(function jogar() {
-                    div.innerHTML = contador++
-                }, 1000)
-            }
-        }
+function play() {
+    if (id == null) {
+        id = setInterval(function jogar() {
+            div.innerHTML = contador++
+        }, 1000)
+    }
 
-        function stop() {
-            clearInterval(id)
-            id = null
-        }
+}
 
-        function reload() {
-            location.reload()
-        }
+function stop() {
+    clearInterval(id)
+    clearInterval(mili)
+    id = null
+    mili = null
+
+}
+
+function reload() {
+    location.reload()
+}
